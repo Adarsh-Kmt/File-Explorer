8c3fa2371593609fe0ef6e0caab77bfb35f2baf0
Adarsh Kamath <adarshkmt2004@gmail.com>
2024-03-11 11:26:19 +0530

    added lombok annotations to response objects.

--- src/main/java/kamathadarsh/FileExplorer/response/CustomResponse.java
+++ src/main/java/kamathadarsh/FileExplorer/response/CustomResponse.java
@@ -1,4 +1,12 @@
 package kamathadarsh.FileExplorer.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+
+@Data
 public class CustomResponse {
 }
--- src/main/java/kamathadarsh/FileExplorer/response/FailureResponse.java
+++ src/main/java/kamathadarsh/FileExplorer/response/FailureResponse.java
@@ -2,12 +2,15 @@ package kamathadarsh.FileExplorer.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
+
 public class FailureResponse extends CustomResponse{
 
     private String errorString;

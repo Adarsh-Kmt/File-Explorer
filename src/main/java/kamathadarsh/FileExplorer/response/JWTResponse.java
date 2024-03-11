418eaa1e671f1d6f9779c844d7813d94125bf7a7
Adarsh Kamath <adarshkmt2004@gmail.com>
2024-03-11 11:35:19 +0530

    added annotations for jwt Response class

--- src/main/java/kamathadarsh/FileExplorer/response/JWTResponse.java
+++ src/main/java/kamathadarsh/FileExplorer/response/JWTResponse.java
@@ -2,11 +2,15 @@ package kamathadarsh.FileExplorer.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @AllArgsConstructor
 @Builder
+@Data
+@NoArgsConstructor
 public class JWTResponse {
 
     public String jwtToken;
